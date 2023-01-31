89ac1ec1a239f6f4f7ae894e786263947d097916
carlosmatsumoto <carlos.matsumoto@bexsbanco.com.br>
2023-01-31 09:53:01 -0300

    Initial commit

---
+++ features/report.feature
@@ -0,0 +1,8 @@
+Feature: Test report generation
+
+Background:
+  Given I generate a test report
+
+
+Scenario: generate a test report
+  Given I generate another test report
---
+++ features/steps/report_steps.py
@@ -0,0 +1,11 @@
+from behave import given, when, then
+
+
+@given('I generate a test report')
+def step_impl(context):
+    pass
+
+
+@given('I generate another test report')
+def step_impl(context):
+    pass
